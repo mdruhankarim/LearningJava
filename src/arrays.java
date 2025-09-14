b8eae89d3a3e23912fb08de803f9cf5f70d5d3c3
mdruhankarim <mdruhankarim@gmail.com>
2025-09-15 00:59:49 +0600

    Finding Golden Keys

--- src/arrays.java
+++ src/arrays.java
@@ -6,11 +6,18 @@ public class arrays {
         System.out.println(continents[2]);
         System.out.println(continents[3]);
         System.out.println(continents[4]);
-        System.out.println(continents[9]); // There's no index 9!*/
+        System.out.println(continents[9]); // There's no index 9!
         String[] favDrinks = {"Thanda", "Juice", "Shorbot", "Pani", "Borof", "Ice-Cream", "Mojo", "Aro", "Fuck Isarel"};
         System.out.println(favDrinks[2]);
         System.out.println(favDrinks[3]);
         System.out.println(favDrinks[6]);
-        System.out.println(favDrinks.length);
+        System.out.println(favDrinks.length);*/
+        int[] keys = {2348, 2345823, 654, 30458, 5049, 589458, 3248238,  2374, 4234, 87756, 45654, 4654, 4566, 465465, 908, 798, 678, 67856785, 100, 234852, 23485, 456654, 543, 324534, 45678, 354, 345423, 23423, 5685, 98567, 23465, 6987, 456, 456, 798};
+        for(int i=0;i<keys.length;i++) {
+            if(keys[i]==100){
+                System.out.println("You found the golden key!");
+                break;
+            }
+        }
     }
 }
