103f47b0aee6c00bb03ba6ba500301e74e93114d
mdruhankarim <mdruhankarim@gmail.com>
2025-09-15 00:45:55 +0600

    arrays

--- src/arrays.java
+++ src/arrays.java
@@ -1,11 +1,16 @@
 public class arrays {
     public static void main(String[] args){
         //String[] zombieSurvivalPack = {"Water 🥤", "Dagger 🗡️", "Food 🍞", "Bandaids 🩹", "Helmet ⛑️"};
-        String[] continents = {"Asia", "Africa", "North America", "South America", "Australia", "Europe", "Antarctica"};
+        /*String[] continents = {"Asia", "Africa", "North America", "South America", "Australia", "Europe", "Antarctica"};
 
         System.out.println(continents[2]);
         System.out.println(continents[3]);
         System.out.println(continents[4]);
-        System.out.println(continents[9]); // There's no index 9!
+        System.out.println(continents[9]); // There's no index 9!*/
+        String[] favDrinks = {"Thanda", "Juice", "Shorbot", "Pani", "Borof", "Ice-Cream", "Mojo", "Aro", "Fuck Isarel"};
+        System.out.println(favDrinks[2]);
+        System.out.println(favDrinks[3]);
+        System.out.println(favDrinks[6]);
+        System.out.println(favDrinks.length);
     }
 }
