348a88db519807d8f623f2433975668ab4bcd487
mdruhankarim <mdruhankarim@gmail.com>
2025-09-15 00:37:34 +0600

    Today's first commit

---
+++ src/Day1.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class Day1 {
+    public static void main(String[] args){
+        int a;
+        System.out.print("Please Enter a Value: ");
+        Scanner scanner = new Scanner(System.in);
+        a = scanner.nextInt();
+        System.out.println("You Entered: " + a);
+    }
+}
---
+++ src/arrays.java
@@ -0,0 +1,11 @@
+public class arrays {
+    public static void main(String[] args){
+        //String[] zombieSurvivalPack = {"Water 🥤", "Dagger 🗡️", "Food 🍞", "Bandaids 🩹", "Helmet ⛑️"};
+        String[] continents = {"Asia", "Africa", "North America", "South America", "Australia", "Europe", "Antarctica"};
+
+        System.out.println(continents[2]);
+        System.out.println(continents[3]);
+        System.out.println(continents[4]);
+        System.out.println(continents[9]); // There's no index 9!
+    }
+}
